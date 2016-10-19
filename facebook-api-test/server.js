f34bf5523f06c3fb9ec7e79863e3afe99917e82a
Yingray <yingray.lu@fuhu.com>
2016-10-19 14:13:32 +0800

    updated the project

--- facebook-api-test/server.js
+++
@@ -1,6 +0,0 @@
-var express = require('express');
-var app = express();
-app.use(express.static(__dirname + '/build'));
-app.listen(3333, function () {
-    console.log("open http://localhost:3333");
-});
